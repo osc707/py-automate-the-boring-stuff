f18e3a45dd05555a3d803380d9b96d523eee2f2a
Oscar Campos <ocampos@adobe.com>
2025-11-10 21:30:08 -0800

    chapter 1

---
+++ src/chp1/hello.py
@@ -0,0 +1,10 @@
+
+print("Hello, world!")
+print("What is your name?")
+my_name = input(">")
+print("it's good to meet you, " + my_name)
+print("The lenght of your name is: ")
+print(len(my_name))
+print("What is your age?")
+my_age = input(">")
+print("You will be " + str(int(my_age) + 1) + " in a year " + my_name + ".")
